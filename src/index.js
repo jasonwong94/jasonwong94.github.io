621f386b3a942febd202f58e67b278cc4ac40c8a
Jason Wong <jaw.wong@mail.utoronto.ca>
2017-05-21 20:04:26 -0400

    Added some projects to json object. Fixed “extended width” issue for mobile and columns not showing paddings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+var projects = [{
+  "title": "Connect 4",
+  "tag": "Embedded",
+  "description": "In my opinion, one of the best projects I've managed while in Spark."
+}, {
+  "title": "Wall Charging with GaN",
+  "tag": "Hardware",
+  "description": "4th Year Capstone Design Project. For capstone, we designed a USB-C controller, comparing effeciencies of gallium nitride (GaN) vs silicon (Si) transistors"
+}, {
+  "title": "You're Next Career Network",
+  "tag": "Software",
+  "description": "Designed and brought new features to the You're Next website. New features include events, jobs and resources. Developed with Python and jQuery"
+}, {
+  "title": "Android Development",
+  "tag": "Software",
+  "description": "Designing an Android App.."
+}, {
+  "title": "Music Switchboard",
+  "tag": "Embedded",
+  "description": ""
+}]
+
 angular.module("jasonwong94-mainpage", [])
   .controller('mainPageController', ["$http", "$scope", function($http, $scope){
       var content = this;
       content.isDialogOpen = true;
       content.name = undefined;
+      content.projects = projects;
 
       console.debug("Controller loaded!");
 
