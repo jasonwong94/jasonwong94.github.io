d417d20698ee1bf31e2943c47d18c8803a57bc2f
Jason Wong <jaw.wong@mail.utoronto.ca>
2017-06-22 02:02:30 -0400

    Updated util code to generate JSON and css files

--- src/static/css/generate_colour-scheme.py
+++ src/static/css/generate_colour-scheme.py
@@ -3,29 +3,68 @@
 import time
 
 FILE_NAME_IN = 'colours.scss'
-FILE_NAME_OUT = 'colour-test.css'
+FILE_NAME_OUT = 'colour-scheme.css'
+FILE_NAME_OUT_JSON = 'colour.json'
 
-PATTERN = ".%s : {background-color: %s};\n"
+PATTERN = ".%s {background-color: %s}\n"
+PATTERN_JSON = " \'%s\': %s,\n"
 
-file_in = open(FILE_NAME_IN, 'r')
 file_out = open(FILE_NAME_OUT, 'w')
+file_out_json = open(FILE_NAME_OUT_JSON, 'w')
 
-def generateCSSHeader():
-    file_out.write("/* Auto-generated file */\n")
-    file_out.write("/* Last Generated: %s */\n" % (time.strftime("%Y-%m-%d %H:%M") )
+def generateHeader(fileLink):
+    fileLink.write("/* Auto-generated file */\n")
+    fileLink.write("/* Last Generated: %s */\n" % (time.strftime("%Y-%m-%d %H:%M") )
     )
 
-def generateCSS(name, colour):
-    className = name.strip('$')
-    file_out.write(PATTERN % (className, colour) )
+def generateFooter(fileLink):
+    fileLink.write
 
-if __name__ == "__main__":
-    print( "Generating CSS file from %s ... " % (FILE_NAME_IN))
-    generateCSSHeader()
+def generateCSS(name, colour):
+    file_out.write(PATTERN % (name, colour) )
+
+def generateCSSFile(file_out, file_in):
+    generateHeader(file_out)
     for line in file_in.readlines():
         line.strip('\n')
 
         if '$' in line:
             line = line.split(':')
+            line[0] = line[0].strip('$')
             generateCSS(line[0], line[1])
-    print( "...Done. File created: %s" % (FILE_NAME_OUT))
+    print( "...Done. File created: %s" % (file_out))
+
+def generateJSONKey(key, value):
+    if key is not '':
+        file_out_json.write(PATTERN_JSON % (key, value))
+
+def generateJSONfile(file_out, file_in):
+    generateHeader(file_out)
+    file_out.write("{ \n")
+
+    key = ''
+    classArray = []
+
+    for line in file_in.readlines():
+        line.strip('\n')
+
+        if '//' in line:
+            generateJSONKey(key, classArray)
+            classArray = []
+            key = line.strip('//').strip('\n')
+
+        if '$' in line:
+            line = line.strip('$').split(':')
+            classArray.append(line[0])
+
+    file_out.write("} \n")
+    print( "...Done. File created: %s" % (file_out))
+
+
+if __name__ == "__main__":
+    print( "Generating CSS file from %s ... " % (FILE_NAME_IN))
+
+    file_in = open(FILE_NAME_IN, 'r')
+    generateCSSFile(file_out, file_in)
+    file_in = open(FILE_NAME_IN, 'r')
+    generateJSONfile(file_out_json, file_in)
