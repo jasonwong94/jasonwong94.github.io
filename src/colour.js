a8e807a69739154836b37b707541de34fbd94711
Jason Wong <jaw.wong@mail.utoronto.ca>
2017-06-30 20:25:34 -0400

    Added a feature to display if colour util FE cannot load the data

--- src/colour.js
+++ src/colour.js
@@ -3,11 +3,21 @@ angular.module('jasonwong94-colour', [])
     console.info("hello world");
 
     var content = this;
+    content.error = true;
     var URL = "./src/static/css/colour.json"
 
-    $http.get(URL).then(function(response){
-      console.log(response.message);
-      content.data = response.data;
-      content.dataFetched = new Date()
-    })
+    this.loadContent = function(){
+      content.dataFetched = new Date();
+
+      $http.get(URL).then(function(response){
+        console.log(response.message);
+        content.data = response.data;
+        content.error = false;
+      }, function(error){
+        console.log(error);
+        content.error = true;
+      })
+    }
+
+    this.loadContent();
   }])
